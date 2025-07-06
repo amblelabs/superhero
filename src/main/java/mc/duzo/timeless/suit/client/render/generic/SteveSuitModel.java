fd86ce8e255ee7c6bda07c16476ae887f3d30021
James Hall <james.hall01@outlook.com>
2025-07-06 23:20:47 +0100

    api: generic steve & alex models for suits

---
+++ src/main/java/mc/duzo/timeless/suit/client/render/generic/SteveSuitModel.java
@@ -0,0 +1,139 @@
+package mc.duzo.timeless.suit.client.render.generic;
+
+import mc.duzo.timeless.suit.client.ClientSuit;
+import mc.duzo.timeless.suit.client.render.SuitModel;
+import net.minecraft.client.model.*;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.RotationAxis;
+
+public class SteveSuitModel extends SuitModel {
+	private final ClientSuit suit;
+
+	private final ModelPart root;
+	protected final ModelPart head;
+	protected final ModelPart body;
+	protected final ModelPart rightArm;
+	protected final ModelPart leftArm;
+	protected final ModelPart rightLeg;
+	protected final ModelPart leftLeg;
+
+	public SteveSuitModel(ClientSuit suit, ModelPart root) {
+		this.root = root;
+		this.head = root.getChild("head");
+		this.body = root.getChild("body");
+		this.rightArm = root.getChild("right_arm");
+		this.leftArm = root.getChild("left_arm");
+		this.rightLeg = root.getChild("right_leg");
+		this.leftLeg = root.getChild("left_leg");
+
+		this.suit = suit;
+	}
+
+	public SteveSuitModel(ClientSuit suit) {
+		this(suit, getTexturedModelData().createModel());
+	}
+
+	public static TexturedModelData getTexturedModelData() {
+		ModelData modelData = new ModelData();
+		ModelPartData modelPartData = modelData.getRoot();
+		ModelPartData head = modelPartData.addChild("head", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.0F))
+				.uv(32, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.5F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+		ModelPartData body = modelPartData.addChild("body", ModelPartBuilder.create().uv(16, 16).cuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.0F))
+				.uv(16, 32).cuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+		ModelPartData right_arm = modelPartData.addChild("right_arm", ModelPartBuilder.create().uv(40, 16).cuboid(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F))
+				.uv(40, 32).cuboid(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.pivot(-5.0F, 2.0F, 0.0F));
+
+		ModelPartData left_arm = modelPartData.addChild("left_arm", ModelPartBuilder.create().uv(32, 48).cuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F))
+				.uv(48, 48).cuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.pivot(5.0F, 2.0F, 0.0F));
+
+		ModelPartData right_leg = modelPartData.addChild("right_leg", ModelPartBuilder.create().uv(0, 16).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F))
+				.uv(0, 32).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.pivot(-1.9F, 12.0F, 0.0F));
+
+		ModelPartData left_leg = modelPartData.addChild("left_leg", ModelPartBuilder.create().uv(16, 48).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F))
+				.uv(0, 48).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.25F)), ModelTransform.pivot(1.9F, 12.0F, 0.0F));
+		return TexturedModelData.of(modelData, 64, 64);
+	}
+
+	@Override
+	public void render(LivingEntity entity, float tickDelta, MatrixStack matrices, VertexConsumer vertexConsumers, int light, float r, float g, float b, float alpha) {
+		matrices.push();
+		if (!(entity instanceof AbstractClientPlayerEntity)) {
+			matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(180f));
+		}
+
+		this.getPart().render(matrices, vertexConsumers, light, OverlayTexture.DEFAULT_UV, r, g, b, alpha);
+		matrices.pop();
+	}
+
+	@Override
+	public void renderArm(boolean isRight, AbstractClientPlayerEntity player, int i, MatrixStack matrices, VertexConsumer buffer, int light, int i1, int i2, int i3, int i4) {
+		if (isRight) this.renderRightArm(player, i, matrices, buffer, light, i1, i2, i3, i4);
+		else this.renderLeftArm(player, i, matrices, buffer, light, i1, i2, i3, i4);
+	}
+
+	private void renderRightArm(AbstractClientPlayerEntity player, int i, MatrixStack matrices, VertexConsumer buffer, int light, int i1, int i2, int i3, int i4) {
+		matrices.push();
+
+		this.rightArm.resetTransform();
+		matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(-0.5f));
+		this.rightArm.render(matrices, buffer, light, OverlayTexture.DEFAULT_UV, 1f, 1f, 1f, 1f);
+
+		matrices.pop();
+	}
+
+	private void renderLeftArm(AbstractClientPlayerEntity player, int i, MatrixStack matrices, VertexConsumer buffer, int light, int i1, int i2, int i3, int i4) {
+		matrices.push();
+
+		this.leftArm.resetTransform();
+		this.leftArm.render(matrices, buffer, light, OverlayTexture.DEFAULT_UV, 1f, 1f, 1f, 1f);
+
+		matrices.pop();
+	}
+
+	@Override
+	public void setVisibilityForSlot(EquipmentSlot slot) {
+		switch (slot) {
+			case HEAD -> {
+				this.head.visible = true;
+				this.body.visible = false;
+				this.rightArm.visible = false;
+				this.leftArm.visible = false;
+				this.rightLeg.visible = false;
+				this.leftLeg.visible = false;
+			}
+			case CHEST -> {
+				this.head.visible = false;
+				this.body.visible = true;
+				this.rightArm.visible = true;
+				this.leftArm.visible = true;
+				this.rightLeg.visible = false;
+				this.leftLeg.visible = false;
+			}
+			case LEGS, FEET -> {
+				this.head.visible = false;
+				this.body.visible = false;
+				this.rightArm.visible = false;
+				this.leftArm.visible = false;
+				this.rightLeg.visible = true;
+				this.leftLeg.visible = true;
+			}
+		}
+	}
+
+	@Override
+	public ClientSuit getSuit() {
+		return suit;
+	}
+
+	@Override
+	public ModelPart getPart() {
+		return root;
+	}
+}
