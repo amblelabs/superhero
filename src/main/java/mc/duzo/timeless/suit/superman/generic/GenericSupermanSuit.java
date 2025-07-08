dd262ae8619ba9f17019e67543d6526ebfaf961e
James Hall <james.hall01@outlook.com>
2025-07-08 23:44:45 +0100

    fix: invisible arms on superman

--- src/main/java/mc/duzo/timeless/suit/superman/generic/GenericSupermanSuit.java
+++ src/main/java/mc/duzo/timeless/suit/superman/generic/GenericSupermanSuit.java
@@ -1,5 +1,6 @@
 package mc.duzo.timeless.suit.superman.generic;
 
+import mc.duzo.animation.generic.VisiblePart;
 import mc.duzo.timeless.power.PowerList;
 import mc.duzo.timeless.power.PowerRegistry;
 import mc.duzo.timeless.suit.client.ClientSuit;
@@ -8,7 +9,11 @@ import mc.duzo.timeless.suit.client.render.generic.SteveSuitModel;
 import mc.duzo.timeless.suit.set.SetRegistry;
 import mc.duzo.timeless.suit.set.SuitSet;
 import mc.duzo.timeless.suit.superman.SupermanSuit;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.entity.LivingEntity;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class GenericSupermanSuit extends SupermanSuit {
@@ -23,11 +28,17 @@ public class GenericSupermanSuit extends SupermanSuit {
 	}
 
 	@Override
+	@Environment(EnvType.CLIENT)
 	protected ClientSuit createClient() {
 		return new ClientSuit(this) {
 			@Override
 			public Supplier<SuitModel> model() {
 				return () -> new SteveSuitModel(this);
+			}
+
+			@Override
+			public List<VisiblePart> getVisibleParts(LivingEntity entity) {
+				return List.of(VisiblePart.HEAD, VisiblePart.LEFT_ARM, VisiblePart.LEFT_SLEEVE, VisiblePart.RIGHT_ARM, VisiblePart.RIGHT_SLEEVE);
 			}
 		};
 	}
